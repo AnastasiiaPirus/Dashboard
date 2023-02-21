d9c4eaa876b9cc0f667b3d1e718162e83ca24cb4
AnastasiiaPirus <a.pirus2000@gmail.com>
2023-02-21 16:54:10 -0500

    Clear commented data.

--- src/Card/Childs/PieChartCustom.js
+++ src/Card/Childs/PieChartCustom.js
@@ -1,12 +1,6 @@
 import React, { PureComponent } from 'react';
 import { PieChart, Pie, Sector, ResponsiveContainer, Cell } from 'recharts';
 
-// export default function Child() {
-//   return (
-//     <div>Child</div>
-//   )
-// }
-
 const COLORS = ['#6bfdd0', '#523941', '#a277fb'];
 
 const data = [
@@ -17,36 +11,14 @@ const data = [
 
 const data01 = [
     { name: 'Group A', value: 400 },
-    // { name: 'Group B', value: 300 },
-    // { name: 'Group C', value: 300 },
-    // { name: 'Group D', value: 200 },
+
   ];
-  // const data02 = [
-  //   { name: 'A1', value: 100 },
-  //   { name: 'A2', value: 300 },
-  //   { name: 'B1', value: 100 },
-  //   { name: 'B2', value: 80 },
-  //   { name: 'B3', value: 40 },
-  //   { name: 'B4', value: 30 },
-  //   { name: 'B5', value: 50 },
-  //   { name: 'C1', value: 100 },
-  //   { name: 'C2', value: 200 },
-  //   { name: 'D1', value: 150 },
-  //   { name: 'D2', value: 50 },
-  // ];
+
 
 const renderActiveShape = (props) => {
-  // const RADIAN = Math.PI / 180;
+
   const { cx, cy,  innerRadius, outerRadius, startAngle, endAngle, fill, payload, } = props;
-  // const sin = Math.sin(-RADIAN * midAngle);
-  // const cos = Math.cos(-RADIAN * midAngle);
-  // const sx = cx + (outerRadius + 10) * cos;
-  // const sy = cy + (outerRadius + 10) * sin;
-  // const mx = cx + (outerRadius + 30) * cos;
-  // const my = cy + (outerRadius + 30) * sin;
-  // const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  // const ey = my;
-  // const textAnchor = cos >= 0 ? 'start' : 'end';
+
 
   return (
     <g>
@@ -71,12 +43,7 @@ const renderActiveShape = (props) => {
         outerRadius={outerRadius + 10}
         fill={fill}
       />
-      {/* <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" /> */}
-      {/* <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" /> */}
-      {/* <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill={fill}>{`PV ${value}`}</text>
-      <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#dcc4c4">
-        {`(${(percent * 100).toFixed(2)}%)`}
-      </text> */}
+
     </g>
   );
 };
